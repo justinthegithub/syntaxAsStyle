8f5e066347494dadcaf7ff8921152aaca31b43d6
J <justintheemailaddress@gmail.com>
2024-01-07 17:12:28 -0600

    4 functions

---
+++ shortSentences.js
@@ -0,0 +1,12 @@
+//equations with be
+//equations with linking verbs
+//intransitives
+//transitives 
+
+
+function toBe(noun,anothernoun,verb="be"){
+    console.log(`${noun} ${verb} ${anothernoun}`)
+
+}
+
+toBe("cow", "fun")
